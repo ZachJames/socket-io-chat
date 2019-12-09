398a88d24e910f6dddcb68ecc5bd3e6588b231f6
Zach <zjwgibsg@gmail.com>
2019-12-08 22:46:47 -0500

    Socket events (not working)

--- public/main.js
+++ public/main.js
@@ -1,35 +1,94 @@
 $(function() {
-  const selectUsernamePage = $(".select-username");
-  const chatPage = $(".chat");
+  const selectUsernamePage = $('.select-username')
+  const chatPage = $('.chat')
+  const usernameInput = $('.username-input')
+  const messageInput = $('.message-input')
+  const messages = $('.messages')
+  const usernameDisplay = $('.username-display')
+  const userCount = $('.user-count')
 
-  // Input fields
-  const usernameInput = $(".username-input");
-  const usernameDisplay = $(".username-display");
+  let username = null
+  let usernameColor = null
 
-  const socket = io();
-  let username = null;
+  const socket = io()
+  const colors = [
+    '#e21400',
+    '#91580f',
+    '#f8a700',
+    '#f78b00',
+    '#58dc00',
+    '#287b00',
+    '#a8f07a',
+    '#4ae8c4',
+    '#3b88eb',
+    '#3824aa',
+    '#a700ff',
+    '#d300e7',
+  ]
 
-  const addChatLogMessage = (message, options) => {
-    if (!options) {
-      options = {};
-    }
-  };
+  const addMessageToChat = ({ message, username }) => {
+    messages.append(
+      `<li><span class="message-username" style="color: ${usernameColor}">${username}:</span>${message}</li>`
+    )
+  }
 
   // Login the user
-  const login = username => {
-    username = usernameInput.val().trim();
+  const login = () => {
+    username = usernameInput.val().trim()
 
     if (username.length > 0) {
-      socket.emit("login user", username);
-      usernameDisplay.text(username);
-      selectUsernamePage.hide();
-      chatPage.show();
+      socket.emit('user logged in', username)
+      usernameDisplay.text(username)
+      usernameColor = getRandomColor()
+      selectUsernamePage.hide()
+      chatPage.show()
+      messageInput.focus()
     }
-  };
+  }
+
+  const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)]
 
   // When user submits username form
-  $(".username-form").submit(e => {
-    e.preventDefault();
-    login(username);
-  });
-})();
+  $('.username-form').submit(e => {
+    e.preventDefault()
+    login()
+  })
+
+  // When user submits a new message
+  $('.message-form').submit(e => {
+    e.preventDefault()
+    const message = $('.message-input')
+      .val()
+      .trim()
+    socket.emit('new message', { message, username })
+  })
+
+  /* Socket events */
+
+  // Whenever the server emits 'login', log the login message
+  socket.on('user logged in', ({ numUsersInRoom }) => {
+    // connected = true
+    // Display the welcome message
+    // var message = 'Welcome to Socket.IO Chat – '
+    // log(message, {
+    //   prepend: true,
+    // })
+  })
+
+  socket.on('update number of users', ({ numUsersInRoom }) => {
+    let text = ''
+    if (numUsersInRoom === 1) {
+      text += '1 user online'
+    } else {
+      text += `${numUsersInRoom} users online`
+    }
+    console.log(text)
+    userCount.text(text)
+  })
+
+  socket.on('new message', ({ message, username }) => {
+    addMessageToChat({ message, username })
+  })
+
+  usernameInput.focus()
+})()
--- server.js
+++ server.js
@@ -1,20 +1,55 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const express = require('express')
+const path = require('path')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')))
 
-let numUsersInRoom = 0;
+let numUsersInRoom = 0
 
-io.on("connection", socket => {
-  let userAdded = false;
+io.on('connection', socket => {
+  let thisUserLoggedIn = false
 
-  socket.on("login user", username => {
-    console.log(username);
-  });
-});
+  // When a user logs in, store the username in socket session
+  // and increment num users in room
+  socket.on('user logged in', username => {
+    if (thisUserLoggedIn) return
 
-const PORT = 3000;
-http.listen(PORT, () => console.log(`> Server live on port ${PORT}`));
+    socket.username = username
+    numUsersInRoom++
+    thisUserLoggedIn = true
+    socket.broadcast.emit('update number of users', { numUsersInRoom })
+  })
+
+  // When someone is typing
+  socket.on('user typing', () => {
+    socket.broadcast.emit('user typing', {
+      username: socket.username,
+    })
+  })
+
+  // When someone stops typing
+  socket.on('user stopped typing', () => {
+    socket.broadcast.emit('user stopped typing', {
+      username: socket.username,
+    })
+  })
+
+  socket.on('new message', ({ message, username }) => {
+    socket.emit('new message', { message, username })
+  })
+
+  socket.on('disconnect', () => {
+    if (thisUserLoggedIn) {
+      numUsersInRoom--
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsersInRoom: numUsersInRoom,
+      })
+    }
+  })
+})
+
+const PORT = 3000
+http.listen(PORT, () => console.log(`> Server live on port ${PORT}`))
